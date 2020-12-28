5e05ff5e3beacc68c632f2ab5b6c69d007d6f0dc
ZhongZhan <zhongzhan@weeget.cn>
2020-12-28 14:35:16 +0800

    fix: update android_ci_script.sh.

--- scripts/android_ci_script.sh
+++ scripts/android_ci_script.sh
@@ -9,10 +9,6 @@ flutter build aar
 popd
 
 echo "== Testing General on Flutter's ${FLUTTER_VERSION} channel =="
-pushd General
-
 ./scripts/recursive.sh clean assembleAlphaRelease
-
-popd
 
 echo "-- Success --"
